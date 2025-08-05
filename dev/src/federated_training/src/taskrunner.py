66fe03e8f5efc54a377fe5476ae459c0fd9fb2a7
chiawei <chiawei@symptomtrace.com>
2025-08-05 18:11:00 +0800

    wip

--- dev/src/federated_training/src/taskrunner.py
+++ dev/src/federated_training/src/taskrunner.py
@@ -126,9 +126,9 @@ class TemplateTaskRunner(PyTorchTaskRunner):
         # Store losses and metrics
         train_loss_key = list(train_logs.keys())[0] # first key is for loss
 
-        self.store_train_loss.append(train_logs[train_loss_key].item())
-        self.store_train_iou.append(train_logs["iou_score"].item())
-        self.store_train_dice.append(train_logs["fscore"].item())
+        self.store_train_loss.append(train_logs[train_loss_key])
+        self.store_train_iou.append(train_logs["iou_score"])
+        self.store_train_dice.append(train_logs["fscore"])
 
         save_dir = "save"
         os.makedirs(save_dir, exist_ok=True)
@@ -173,13 +173,13 @@ class TemplateTaskRunner(PyTorchTaskRunner):
         # Store losses and metrics
         val_loss_key = list(valid_logs.keys())[0] # first key is for loss
 
-        self.store_val_loss.append(valid_logs[val_loss_key].item())
-        self.store_val_iou.append(valid_logs["iou_score"].item())
-        self.store_val_dice.append(valid_logs["fscore"].item())
+        self.store_val_loss.append(valid_logs[val_loss_key])
+        self.store_val_iou.append(valid_logs["iou_score"])
+        self.store_val_dice.append(valid_logs["fscore"])
 
         # Track best performance, and save the model's state
         if  self.best_vloss > valid_logs[val_loss_key]:
-            self.best_vloss = valid_logs[val_loss_key].item()
+            self.best_vloss = valid_logs[val_loss_key]
             logger.info(f'Validation loss reduced. Saving the model.')
             self.cnt_patience = 0 # reset patience
             # best_model_epoch = self.epoch
@@ -187,7 +187,7 @@ class TemplateTaskRunner(PyTorchTaskRunner):
 
         # Compare iou score
         elif self.best_viou < valid_logs['iou_score']:
-            self.best_viou = valid_logs['iou_score'].item()
+            self.best_viou = valid_logs['iou_score']
             logger.info(f'Validation IoU increased. Saving the model at epoch.')
             self.cnt_patience = 0 # reset patience
             # best_model_epoch = self.epoch
